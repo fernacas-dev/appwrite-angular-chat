4a79f760221c71a954da295928a8ba256398afbb
Fernando Castellanos <fernan2cas96@gmail.com>
2024-06-02 11:52:30 -0300

    updated to angular v18

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,3 @@
 export const environment = {
-    endpoint: 'https://cloud.appwrite.io/v1',
-    projectId: 'chat-demo',
-    databaseId: 'chatdb',
-    chatCollectionId: 'messages',
-    prod: true
- };
+
+};
